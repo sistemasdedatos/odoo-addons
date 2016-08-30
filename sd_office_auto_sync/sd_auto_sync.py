5f82b79a597524042e8abd395dc86321db706ad6
Rodrigo Colombo <rodrigo.covl@gmail.com>
2016-08-30 14:28:40 +0100

    V3 de los modulos de sincronizacion, subido a produccion en version beta

--- sd_office_auto_sync/sd_auto_sync.py
+++ sd_office_auto_sync/sd_auto_sync.py
@@ -7,39 +7,51 @@ from matplotlib.cbook import Null
 import datetime
 import time
 
+
 class calendar_event(models.Model):
     _inherit = "calendar.event"
     
+    last_start = fields.Datetime('Last start', default = datetime.datetime.now (), readonly = True)
+    last_stop = fields.Datetime('Last stop', default = datetime.datetime.now (), readonly = True)
+    
     @api.multi
     def write (self, val):
-        last_start = self.start
-        last_stop = self.stop
-        super (calendar_event, self).write (val)
-        office_config = self.env['sd.office.config'].search ([('create_uid', '=', self._uid)])
-        if len (office_config) > 0 and office_config.confirmed:
-            start = last_start if last_start <= self.start else self.start
-            stop = last_stop if last_stop >= self.stop else self.stop
-            start = datetime.datetime.strptime (start, '%Y-%m-%d %H:%M:%S') - datetime.timedelta (days = 1)
-            stop = datetime.datetime.strptime (stop, '%Y-%m-%d %H:%M:%S') + datetime.timedelta (days = 1)
-            office_sync = self.env['sd.office.sync'].create ({'sd_office_config_id': office_config.id,
-                                                              'date_init': start,
-                                                              'date_end':  stop})
-            office_sync.sync_events (context = {'DirSync': 'ToOffice'}, odoo_events = self)
-        return True
-    
+        print val
+        if 'start_datetime' in val.keys ():
+            self.last_start = self.start
+        if 'stop_datetime' in val.keys ():
+            self.last_stop = self.stop
+        start = self.last_start if self.last_start <= self.start else self.start
+        stop = self.last_stop if self.last_stop >= self.stop else self.stop
+        start = datetime.datetime.strptime (start, '%Y-%m-%d %H:%M:%S') - datetime.timedelta (days = 1)
+        stop = datetime.datetime.strptime (stop, '%Y-%m-%d %H:%M:%S') + datetime.timedelta (days = 1)
+        office_sync = self.env['sd.office.sync'].create ({'sd_office_config_id': False,
+                                                          'date_init': start,
+                                                          'date_end':  stop})
+         
+        office_sync.sync_events (context = {'DirSync': 'ToOffice'}, odoo_events = self )
+        return super (calendar_event, self).write (val)
+     
     @api.multi
     def unlink (self):
-        office_config = self.env['sd.office.config'].search ([('create_uid', '=', self._uid)])
+        print self.attendee_ids
         for i in self:
-            if i.office_id and len (office_config) > 0 and office_config.confirmed:
-                office_sync = self.env['sd.office.sync'].create ({'sd_office_config_id': office_config.id,
-                                                                  'date_init': i.start,
-                                                                  'date_end':  i.stop})
-                office_sync.delete_office (i)
+            office_sync = self.env['sd.office.sync'].create ({'sd_office_config_id': False,
+                                                              'date_init': i.start,
+                                                              'date_end':  i.stop})
+            delete_office = office_sync.delete_office (i, self.attendee_ids)
         return super (calendar_event, self).unlink ()
     
-    @api.multi    
-    def copy (self, default):
-        default['office_id'] = False
-        return super(calendar_event, self).copy (default)
+class calendar_attendee(models.Model):
     
+    _inherit = 'calendar.attendee'
+    
+    @api.multi
+    def do_accept(self):
+        start = datetime.datetime.strptime (self.event_id.start, '%Y-%m-%d %H:%M:%S') - datetime.timedelta (days = 1)
+        stop = datetime.datetime.strptime (self.event_id.stop, '%Y-%m-%d %H:%M:%S') + datetime.timedelta (days = 1)
+        office_sync = self.env['sd.office.sync'].create ({'sd_office_config_id': False,
+                                                          'date_init': start,
+                                                          'date_end':  stop})
+        office_sync.sync_events (context = {'DirSync': 'ToOffice'}, odoo_events = self.event_id)
+        return super (calendar_attendee, self).do_accept ()
