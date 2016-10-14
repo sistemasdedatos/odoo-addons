e4ee1d0ce8021f5d1b3c39b3035579d8a4edb886
Rodrigo Colombo <rodrigo.covl@gmail.com>
2016-10-14 16:04:48 +0100

    Modulo sd_user_warehouse primera version, en la ficha del usuario puedes seleccionar un almacen, si no se pone nada se establece el por defecto

---
+++ sd_user_warehouse/__openerp__.py
@@ -0,0 +1,45 @@
+# -*- coding: utf-8 -*-
+##############################################################################
+#
+#    OpenERP, Open Source Management Solution
+#    Copyright (C) 2016 Sistemas de Datos (<http://www.sdatos.com>).
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU Affero General Public License as
+#    published by the Free Software Foundation, either version 3 of the
+#    License, or (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU Affero General Public License for more details.
+#
+#    You should have received a copy of the GNU Affero General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+##############################################################################
+
+{
+    'name' : 'SDatos User Warehouse',
+    'version' : '0.1',
+    'author' : 'Sistemas de Datos',
+    'maintainer': 'Sistemas de Datos',
+    'category' : 'Warehouse Management',
+    'summary': 'A Warehouse for user',
+    'description' : """
+User Warehouse
+==============
+
+This module allows define a preferred warehouse for each salesman/user
+    """,
+    'website': 'http://www.sdatos.com',
+    # End General Data
+    'depends' : ['base', 
+                 'sale', 
+                 'stock', 
+                 'sale_stock'],        
+    'data': ['views/user_view.xml'],  
+    'installable': True,        
+    'auto_install': False,        
+    'application': False,
+}
