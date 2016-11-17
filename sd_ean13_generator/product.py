3b5b2c8d3e2c763175787ced5d63ce130f401c7a
Rodrigo Colombo <rodrigo.covl@gmail.com>
2016-11-17 08:41:10 +0000

    Segunda version del modulo sd_ean13_generator, con la utilidad dentro de la plantilla de productos. Ahora es posible establecer ean a plantillas en concreto

--- sd_ean13_generator/product.py
+++ sd_ean13_generator/product.py
@@ -23,44 +23,53 @@ class product_template(models.Model):
     
     @api.multi
     def generate_ean13 (self, count = 0):
-        if self.default_code:
-            m1 = str (self.default_code)[0]
-            m2 = str (self.default_code)[1:len(self.default_code)]
-            ean = m1
-            ean += str ('%.2d' % abs (self.list_price)).split ('.')[0] + '7' + str ('%.2d' % int (float (str('%.2f' % abs (self.list_price)).split ('.')[1])))
-            ean += m2
-        else:
-            ean = "0"
-            ean += str ('%.2d' % abs (self.list_price)).split ('.')[0] + '7' + str ('%.2d' % int (float (str('%.2f' % abs (self.list_price)).split ('.')[1])))    
-        if len (ean) >= 13:
-            if len (str (count)) > 1:
-                ean = ean[:10] + str (count) + ean[10:]
+        for s in self:
+            if s.default_code:
+                m1 = str (s.default_code)[0]
+                m2 = str (s.default_code)[1:len(s.default_code)]
+                ean = m1
+                ean += str ('%.2d' % abs (s.list_price)).split ('.')[0] + '7' + str ('%.2d' % int (float (str('%.2f' % abs (s.list_price)).split ('.')[1])))
+                ean += m2
             else:
-                ean = ean[:11] + str (count) + ean[11:]
-        else:
-            ean += str (count)
-        print ean
-        return openerp.addons.product.product.sanitize_ean13 (ean)
+                ean = "0"
+                ean += str ('%.2d' % abs (s.list_price)).split ('.')[0] + '7' + str ('%.2d' % int (float (str('%.2f' % abs (s.list_price)).split ('.')[1])))    
+            if len (ean) >= 12:
+                if len (str (count)) > 1:
+                    ean = ean[:10] + str (count) + ean[10:]
+                else:
+                    ean = ean[:11] + str (count) + ean[11:]
+            else:
+                ean += str (count)
+            ean = openerp.addons.product.product.sanitize_ean13 (ean)
+            if s.env['product.product'].search ([('ean13', '=', ean)]):  #, ('active', '=', True)
+                ean = s.generate_ean13 (count + 50)
+        
+        return ean
+    
+class product_product(models.Model):
+    _inherit = "product.product"
     
     @api.multi
-    def create_variant_ids(self):
-        ean13 = self.ean13
-        ref = self.default_code
-        print ref
-        product_variant_count = self.product_variant_count
-        if not ean13:
-            ean13 = self.generate_ean13 ()
-        super (product_template, self).create_variant_ids ()
-        tmpl_ids = self.browse (self._ids)
-        for tmpl_id in tmpl_ids:
-            count = product_variant_count + 1 if product_variant_count > 1 else product_variant_count
-            print len (tmpl_id.product_variant_ids) 
-            print product_variant_count
-            if len (tmpl_id.product_variant_ids) > 1 and product_variant_count < len (tmpl_id.product_variant_ids):
-                for product_id in tmpl_id.product_variant_ids:
-                    if not product_id.ean13:
-                        print "anterior", product_id.ean13
-                        product_id.ean13 = self.generate_ean13 (count)
-                        count = count + 1
-                    print product_id.ean13
-                    
+    def generate_ean13 (self, count = 0):
+        for s in self:
+            if s.default_code:
+                ean = str (s.default_code)[0]
+                ean += str ('%.2d' % abs (s.lst_price)).split ('.')[0] + '7' + str ('%.2d' % int (float (str('%.2f' % abs (s.lst_price)).split ('.')[1])))
+                ean += str (s.default_code)[1:len(s.default_code)]
+            else:
+                ean = str (s.id)[0]
+                ean += str ('%.2d' % abs (s.lst_price)).split ('.')[0] + '7' + str ('%.2d' % int (float (str('%.2f' % abs (s.lst_price)).split ('.')[1])))
+                ean += str (s.id)[1:len (str (s.id))]    
+            if len (ean) >= 12:
+                if len (str (count)) > 1:
+                    ean = ean[:10] + str (count) + ean[10:]
+                else:
+                    ean = ean[:11] + str (count) + ean[11:]
+            else:
+                ean += str (count)
+            ean = openerp.addons.product.product.sanitize_ean13 (ean)
+            if not s.env['product.product'].search ([('ean13', '=', ean)]):  #, ('active', '=', True)
+                s.ean13 = ean
+            else:    
+                ean = s.generate_ean13 (count + 50)
+            count = count + 1
