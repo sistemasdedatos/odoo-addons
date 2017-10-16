95acd605056f0241cd188f7ef1443c1d82bf696d
Rodrigo Colombo <rodrigo.covl@gmail.com>
2017-10-16 15:26:52 +0100

    Modulo sd_custom_translated v0.2

--- sd_custom_translated/__openerp__.py
+++ sd_custom_translated/__openerp__.py
@@ -21,7 +21,7 @@
 
 {
     'name' : 'Custom Translations',
-    'version' : '0.1',
+    'version' : '0.2',
     'author' : 'Sistemas de Datos',
     'maintainer': 'Sistemas de Datos',
     'category' : 'Interface',
