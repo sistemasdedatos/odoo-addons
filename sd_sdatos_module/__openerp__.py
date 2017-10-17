8b192f583ed80f7a2a86c0f7338fbd28597fa7cf
Rodrigo Colombo <rodrigo.covl@gmail.com>
2017-10-17 13:02:42 +0100

    Modulo sd_user_warehouse eliminado fichero sin uso. Modulo sd_sdatos_module vista search en tareas para buscar numero de parte fisico o id

--- sd_sdatos_module/__openerp__.py
+++ sd_sdatos_module/__openerp__.py
@@ -21,7 +21,7 @@
 
 {
     'name' : 'SDatos Sistemas de Datos',
-    'version' : '3.9',
+    'version' : '3.9.1',
     'author' : 'Sistemas de Datos',
     'maintainer': 'Sistemas de Datos',
     'category' : 'Interface',
