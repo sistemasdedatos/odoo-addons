8c0b1145f5daf47587c54aa81b8f789d6fa4ad96
Rodrigo Colombo <rodrigo.covl@gmail.com>
2017-10-18 09:24:25 +0100

    Merge modulo sd_sale_no_warning_stock v0.1

---
+++ sd_sale_no_warning_stock/__openerp__.py
@@ -0,0 +1,46 @@
+# -*- coding: utf-8 -*-
+##############################################################################
+#
+#    OpenERP, Open Source Management Solution
+#    Copyright (C) 2017 Sistemas de Datos (<http://www.sdatos.com>).
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU Affero General Public License as
+#    published by the Free Software Foundation, either version 3 of the
+#    License, or (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU Affero General Public License for more details.
+#
+#    You should have received a copy of the GNU Affero General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+##############################################################################
+{
+    'name' : 'SDatos Sale No Warning Stock',
+    'version' : '0.1',
+    'author' : 'Sistemas de Datos',
+    'maintainer': 'Sistemas de Datos',
+    'category' : 'Sales',
+    'summary': 'No sale warning stock',
+    'description' : """
+Sale No Warning Stock
+=====================
+
+Module that modifies the product workflow when you do a sale order
+------------------------------------------------------------------
+The message "Stock not available" is ignored. This module is temporal, the correct method is establish the product as "consumible"
+
+    """,
+    'website': 'http://www.sdatos.com',
+    'depends' : ['sale',
+                 'stock',
+                 'sale_stock'],
+    'data': [],
+    'qweb': [],
+    'installable': True,
+    'auto_install': False,        
+    'application': False,
+}
---
+++ sd_sale_no_warning_stock/models/sale.py
@@ -0,0 +1,37 @@
+# -*- coding: utf-8 -*-
+##############################################################################
+#
+#    OpenERP, Open Source Management Solution
+#    Copyright (C) 2017 Sistemas de Datos (<http://www.sdatos.com>).
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU Affero General Public License as
+#    published by the Free Software Foundation, either version 3 of the
+#    License, or (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU Affero General Public License for more details.
+#
+#    You should have received a copy of the GNU Affero General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+##############################################################################
+from openerp import SUPERUSER_ID
+from openerp import models, api
+
+class sale_order_line (models.Model):
+    _inherit = 'sale.order.line'
+    
+    @api.multi
+    def product_id_change_with_wh (self, pricelist, product, qty = 0,
+                                   uom = False, qty_uos = 0, uos = False, 
+                                   name = '', partner_id = False, lang = False, 
+                                   update_tax = True, date_order = False, 
+                                   packaging = False, fiscal_position = False, 
+                                   flag = False, warehouse_id = False):
+        res = super(sale_order_line, self).product_id_change_with_wh (pricelist, product, qty, uom, qty_uos, uos, name, partner_id, lang, 
+                                                                   update_tax, date_order, packaging, fiscal_position, flag, warehouse_id)
+        res.pop('warning')
+        return res
