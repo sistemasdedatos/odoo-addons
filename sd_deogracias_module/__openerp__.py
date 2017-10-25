1932c400904fa5ba3f029e7b5d1aff1a12ab2b19
Rodrigo Colombo <rodrigo.covl@gmail.com>
2017-10-25 14:33:01 +0100

    Modulo sd_deogracias_module v1.0 modificado para que se concilien los pendientes de cobro

--- sd_deogracias_module/__openerp__.py
+++ sd_deogracias_module/__openerp__.py
@@ -20,7 +20,7 @@
 ##############################################################################
 {
     'name' : 'SDatos Ferreteria Deogracias',
-    'version' : '0.3',
+    'version' : '1.0',
     'author' : 'Sistemas de Datos',
     'maintainer': 'Sistemas de Datos',
     'category' : 'Interface',
--- sd_deogracias_module/models/point_of_sale.py
+++ sd_deogracias_module/models/point_of_sale.py
@@ -18,4 +18,18 @@
 #    along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #
 ##############################################################################
-import res_partner, point_of_sale
+from openerp import models, fields, api
+from openerp.exceptions import Warning
+
+class pos_session (models.Model):
+    _inherit = 'pos.session'
+    
+    @api.multi
+    def wkf_action_close (self):
+        # Close CashBox
+        res = super (pos_session, self).wkf_action_close ()
+        account_id = self.env['account.account'].search([('code', 'like', '430000')])
+        concilie_wizard = self.env['account.automatic.reconcile'].create ({'account_ids': [(6, 0, [account_id.id])],
+                                                                           'power': 2})
+        concilie_wizard.reconcile ()                #Se concilian los fiados de la cuenta 430000
+        return res
