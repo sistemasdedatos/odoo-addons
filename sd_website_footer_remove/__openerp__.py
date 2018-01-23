eccc4903e5fbccb96ce69560e7b530ea576ece44
Rodrigo Colombo <rodrigo.covl@gmail.com>
2018-01-23 16:15:16 +0000

    Modulo sd_website_footer_remove v0.4 borrado el footer custom

--- sd_website_footer_remove/__openerp__.py
+++ sd_website_footer_remove/__openerp__.py
@@ -20,7 +20,7 @@
 ##############################################################################
 {
     'name' : 'SDatos Footer Remove',
-    'version' : '0.3',
+    'version' : '0.4',
     'author' : 'Sistemas de Datos',
     'maintainer': 'Sistemas de Datos',
     'category' : 'Interface',
