121b3ba77ea2ed4f0d1dc4568dd6082d5b7026a8
Rodrigo Colombo <rodrigo.covl@gmail.com>
2018-02-12 13:02:57 +0000

    A

--- sd_globalproj_module/models/account_entries_report.py
+++
@@ -1,6 +0,0 @@
-from openerp import fields, models, api
-
-class account_entries_report(models.Model):
-    _inherit = "account.entries.report"
-    
-    product_default_code = fields.Char(related = 'product_id.default_code')
--- sd_hergore_module/models/account_invoice.py
+++
@@ -1,9 +0,0 @@
-from openerp import models, fields
-
-class account_invoice (models.Model):
-    _name = "account.invoice"
-    _inherit = "account.invoice"
-    
-    sale_order_ids = fields.Many2many ('sale.order', 'sale_order_invoice_rel', 'invoice_id', 'order_id', 'Sales', 
-                                       readonly = True, copy = False, 
-                                       help = "This is the list of invoices that have been generated for this sales order. The same sales order may have been invoiced in several times (by line for example).")
