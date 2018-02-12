a8e5abb733132c9fb782682defb3eb4f04c8ed81
Rodrigo Colombo <rcolombo@sdatos.es>
2018-02-12 13:25:10 +0000

    -

--- sd_slghotel_module/models/project_task.py
+++
@@ -1,8 +0,0 @@
-from openerp import api, models, fields
-from openerp.exceptions import Warning
-from openerp.tools.translate import _
-
-class task (models.Model):
-    _inherit = 'project.task'
-    
-    html_description = fields.Html ("Description")
