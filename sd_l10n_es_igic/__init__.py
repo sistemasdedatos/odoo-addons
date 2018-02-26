0abc4b86cad428e67fefe94c6916836486ebd2ed
Rodrigo Colombo <rodrigo.covl@gmail.com>
2018-02-26 08:35:01 +0000

    Modulo sd_l10n_es_igic modificado, agregado sujeto pasivo en ventas

--- sd_l10n_es_igic/__init__.py
+++ sd_l10n_es_igic/__init__.py
@@ -2,21 +2,9 @@
 ##############################################################################
 #
 #    Sistemas de Datos, Open Source Management Solution
-#    Copyright (C) 2004-2014 Arturo Esparragón Goncalves (http://sdatos.com). All Rights Reserved
-#    
-#
-#    This program is free software: you can redistribute it and/or modify
-#    it under the terms of the GNU General Public License as published by
-#    the Free Software Foundation, either version 3 of the License, or
-#    (at your option) any later version.
-#
-#    This program is distributed in the hope that it will be useful,
-#    but WITHOUT ANY WARRANTY; without even the implied warranty of
-#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-#    GNU General Public License for more details.
-#
-#    You should have received a copy of the GNU General Public License
-#    along with this program.  If not, see http://www.gnu.org/licenses/.
+#    Copyright (C) 2014 Arturo Esparragón Goncalves (http://sdatos.com). All Rights Reserved
+#    Copyright (C) 2016-2018 Rodrigo Colombo Vlaeminch (http://sdatos.com). All Rights Reserved
+#    License AGPL-3 - See http://www.gnu.org/licenses/agpl-3.0
 #
 ##############################################################################
 
