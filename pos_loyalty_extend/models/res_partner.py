753af316d890367d46a663e3dd01bda03bbe7e42
Rodrigo Colombo <rodrigo.covl@gmail.com>
2018-05-07 13:24:22 +0100

    [UPD] pos_loyalty_extend: copyright error

--- pos_loyalty_extend/models/res_partner.py
+++ pos_loyalty_extend/models/res_partner.py
@@ -1,7 +1,6 @@
-# -*- coding: utf-8 -*-
-# Copyright 2004-2010 OpenERP SA
-# Copyright 2017 RGB Consulting S.L. (https://www.rgbconsulting.com)
-# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+# -*- encoding: utf-8 -*-
+#    Copyright 2018 Sistemas de Datos - Rodrigo Colombo Vlaeminch <rcolombo@sdatos.es>
+#    License AGPL-3 - See http://www.gnu.org/licenses/agpl-3.0
 
 from odoo import fields, models, api
 
