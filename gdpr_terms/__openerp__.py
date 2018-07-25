60e37551550289055f667dad6b041e03f4673e3d
Rodrigo Colombo <rodrigo.covl@gmail.com>
2018-07-25 11:07:56 +0100

    [MIG] gdpr_terms: migration from 10.0 to 8.0

---
+++ gdpr_terms/__openerp__.py
@@ -0,0 +1,26 @@
+# -*- encoding: utf-8 -*-
+#    Copyright 2018 Sistemas de Datos - Rodrigo Colombo Vlaeminch <rcolombo@sdatos.es>
+#    License AGPL-3 - See http://www.gnu.org/licenses/agpl-3.0
+{
+    'name' : 'GDPR Terms',
+    'version' : '0.1',
+    'author' : 'Sistemas de Datos',
+    'maintainer': 'Sistemas de Datos',
+    'category' : 'Base',
+    'summary': 'GDPR Terms in footer',
+    'description' : """
+GDPR Terms
+==========
+
+Edit external footer view to show the gdpr terms, this terms should be written in res_company form.
+    """,
+    'website': 'http://www.sdatos.com',
+    # End General Data
+    'depends' : ['base',
+                 'report'],
+    'data': ['views/res_view.xml',
+             'report/layout_templates.xml'],
+    'installable': True,
+    'auto_install': False,
+    'application': False,
+}
