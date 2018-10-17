01c1c7be654b6fd6003fb94cd4b9fa1af5b147ca
Rodrigo Colombo <rodrigo.covl@gmail.com>
2018-10-17 15:35:15 +0100

    [FIX] distribution_price_margin: update lines fixed

--- distribution_price_margin/models/purchase_cost_distribution.py
+++ distribution_price_margin/models/purchase_cost_distribution.py
@@ -45,14 +45,14 @@ class PurchaseCostDistributionLine(models.Model):
     @api.onchange('benefit_price')
     def onchange_benefit_price(self):
         if self.benefit_price >= 0:
-            self.benefit_margin = ((self.benefit_price - self.standard_price_new) / self.standard_price_new) * 100
+            self.benefit_margin = (1 - (self.standard_price_new / self.benefit_price)) * 100
         else:
             self.benefit_margin = -1
     
     @api.onchange('benefit_margin')
     def onchange_benefit_margin(self):
         if self.benefit_margin >= 0:
-            self.benefit_price = self.standard_price_new + (self.standard_price_new * (self.benefit_margin / 100))
+            self.benefit_price = self.standard_price_new / (1 - (self.benefit_margin / 100))
         else:
             self.benefit_price = 0
 
