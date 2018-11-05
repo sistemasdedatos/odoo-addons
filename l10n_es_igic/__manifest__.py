04c6d85b0d0f11ab0cd670082cff4250822563e1
Rodrigo Colombo <rodrigo.covl@gmail.com>
2018-11-05 15:08:20 +0000

    [UPD] l10n_es_igic: add tax group

--- l10n_es_igic/__manifest__.py
+++ l10n_es_igic/__manifest__.py
@@ -10,7 +10,7 @@
 
 {
     'name' : 'Planes de cuentas españoles (según PGCE 2008) ampliado para IGIC',
-    'version' : '0.1',
+    'version' : '1.0',
     'author' : 'Sistemas de Datos',
     'maintainer': 'Sistemas de Datos',
     'category' : 'Localisation/Account Charts',
@@ -18,8 +18,9 @@
     'website': 'http://www.sdatos.com',
     'depends' : ['l10n_es'],            
     'data': ['data/account_account_common_igic.xml',
+             'data/account_tax_group_data.xml',
              'data/taxes_common_igic.xml',   
-             'data/fiscal_position_common_igic.xml'],             
+             'data/fiscal_position_common_igic.xml'],
     'installable': True,        
     'auto_install': False,        
 }
