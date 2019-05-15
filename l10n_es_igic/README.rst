a6c9d896b8dcbd399f30d050c9a94a305e269abc
HéctorJotaPunto <alu0100603170@ull.edu.es>
2019-05-15 10:42:54 +0100

    [MIG] l10n_es_igic 12.0

--- l10n_es_igic/README.rst
+++ l10n_es_igic/README.rst
@@ -1,18 +1,19 @@
-=================================
-IPUESTO GENERAL INDIRECTO CANARIO
-=================================
+==================================
+IMPUESTO GENERAL INDIRECTO CANARIO
+==================================
 
 Módulo que amplía la contabilidad española introduciento los impuestos y posiciones fiscales para el IGIC
 
 Instalación:
 ------------
-En caso de no estar instalado el módulo l10n_es, se debe instalar directamente este módulo.
+En caso de no estar instalado el módulo l10n_es, se debe instalar directamente este módulo y a continuación importar desde el menú de impuestos el archivo "IGIC_tax_group_parents.xls", ubicado en la carpeta "files".
 
 En caso contrario: 
 
 * Instalar este módulo.
 * Instalar account_chart_update (OCA account-financial-tools)
 * Ejecutar el asistente de actualización del plan contable español
+* Importar desde el menú de impuestos el archivo "IGIC_tax_group_parents.xls, ubicado en la carpeta "files"
 
 Módulo disponible en https://github.com/sistemasdedatos/odoo-addons.
 
@@ -22,3 +23,4 @@ Créditos
 Contribuidores
 --------------
 * Rodrigo Colombo <rodrigo.covl@gmail.com> <rcolombo@sdatos.es>
+* Héctor J. Ravelo <hravelo@sdatos.es>
--- l10n_es_igic/__manifest__.py
+++ l10n_es_igic/__manifest__.py
@@ -4,24 +4,26 @@
 #    Sistemas de Datos, Open Source Management Solution
 #    Copyright (C) 2014 Arturo Esparragón Goncalves (http://sdatos.com). All Rights Reserved
 #    Copyright (C) 2016-2018 Rodrigo Colombo Vlaeminch (http://sdatos.com). All Rights Reserved
+#    Copyright (C) 2019 Héctor J. Ravelo (http://sdatos.com). All Rights Reserved
 #    License AGPL-3 - See http://www.gnu.org/licenses/agpl-3.0
 #
 ##############################################################################
 
 {
-    'name' : 'Planes de cuentas españoles (según PGCE 2008) ampliado para IGIC',
-    'version' : '2.0',
-    'author' : 'Sistemas de Datos',
+    'name': 'Planes de cuentas españoles (según PGCE 2008) ampliado para IGIC',
+    'version': '1.0',
+    'author': 'Sistemas de Datos',
     'maintainer': 'Sistemas de Datos',
-    'category' : 'Localisation/Account Charts',
+    'category': 'Localisation/Account Charts',
     'sequence': 3,
     'website': 'http://www.sdatos.com',
-    'depends' : ['l10n_es'],            
-    'data': ['data/account_account_common_igic.xml',
+    'depends': ['l10n_es'],
+    'data': ['data/account_group.xml',
+             'data/account_account_common_igic.xml',
              'data/account_tax_group_data.xml',
-             'data/taxes_common_igic.xml',   
+             'data/taxes_common_igic.xml',
              'data/fiscal_position_common_igic.xml'],
-    'installable': False,        
+    'installable': True,        
     'auto_install': False,        
 }
 
