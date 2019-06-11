42df93413a4ec8a9f0934c45838b9160bae10767
HéctorJotaPunto <alu0100603170@ull.edu.es>
2019-06-11 09:01:35 +0100

    [UPD] distribution_price_margin: modified several files which were incorrect in the last commit

--- distribution_price_margin/__manifest__.py
+++ distribution_price_margin/__manifest__.py
@@ -2,41 +2,47 @@
 ##############################################################################
 #
 #    Sistemas de Datos, Open Source Management Solution
-#    Copyright (C) 2016 Rodrigo Colombo - Sistemas de Datos (<http://www.sdatos.com>).
-#    Migrated v10 2018 Rodrigo Colombo Vlaeminch (http://sdatos.com).
+#    Copyright (C) 2016 Rodrigo Colombo - Sistemas de Datos (<http://www.sdatos.com>)
+#    Migrated v10 2018 Rodrigo Colombo Vlaeminch (http://sdatos.com)
+#    Migrated v12 2019 Héctor J. Ravelo (http://sdatos.com)
 #    License AGPL-3 - See http://www.gnu.org/licenses/agpl-3.0
 #
 ##############################################################################
 
 {
-    'name' : 'Distribution Price Margin',
-    'version' : '2.0',
-    'author' : 'Sistemas de Datos',
+    'name': 'Distribution Price Margin',
+    'version': '1.0',
+    'author': 'Sistemas de Datos',
     'maintainer': 'Sistemas de Datos',
-    'category' : 'Purchases',
-    'summary': 'Introduce margin in distribution cost to established sale price',
-    'description' : """
-Sale Price Margin in Distribution Cost
+    'category': 'Purchases',
+    'summary': 'Introduce margins in cost distribution to set sale price',
+    'description': """
+    
+Sale Price Margin in Cost Distribution
 ======================================
 
-This module allows introduce benefits margin in distribution cost of purchases
+This module allows to introduce benefit margins in purchases cost distribution
 ------------------------------------------------------------------------------
-You can select between two method to calculata sale price\n
+You can choose between two methods to calculate sale price\n
 1: Sale = Cost + (cost * %margin)\n
-1: Sale = Cost / (1 - %margin)\n
+2: Sale = Cost / (1 - %margin)\n
+
+Usage
+-----
+Only users with "Purchase Manager" permission can manage the new options this modules introduces.\n
+To change the calculation method head to Orders section in Purchases/Settings/Settings menu.
+
 Reference: https://manueldelgado.com/como-calcular-el-precio-de-venta-coste-margen/
 
     """,
     'website': 'http://www.sdatos.com',
     # End General Data
-    'depends' : ['sale',
-                 'purchase_landed_cost'],
-    'data': ['security/distribution_margin_method.xml',
-             'views/res_config_view.xml',
+    'depends': ['sale', 'purchase_landed_cost'],
+    'data': ['views/res_config_view.xml',
              'views/purchase_cost_distribution_view.xml'],
-    'images':[],                
-    'installable': False,
-    'auto_install': False,        
+    'images': [],
+    'installable': True,
+    'auto_install': False,
     'application': False,
 }
 
