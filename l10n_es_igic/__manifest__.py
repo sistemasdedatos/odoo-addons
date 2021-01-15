e408e10c17d5d29552480b5bf9f33a292c174107
HéctorJotaPunto <hravelo@sdatos.es>
2021-01-15 12:18:20 +0000

    [UPD] l10n_es_igic: creados todos los records relativos al IGIC 15%

--- l10n_es_igic/__manifest__.py
+++ l10n_es_igic/__manifest__.py
@@ -4,19 +4,20 @@
 #    Sistemas de Datos, Open Source Management Solution
 #    Copyright (C) 2014 Arturo Esparragón Goncalves (http://sdatos.com). All Rights Reserved
 #    Copyright (C) 2016-2018 Rodrigo Colombo Vlaeminch (http://sdatos.com). All Rights Reserved
+#    Copyright (C) 2021 Héctor J. Ravelo (http://sdatos.com)- All Rights Reserved
 #    License AGPL-3 - See http://www.gnu.org/licenses/agpl-3.0
 #
 ##############################################################################
 
 {
-    'name' : 'Planes de cuentas españoles (según PGCE 2008) ampliado para IGIC',
-    'version' : '2.0',
-    'author' : 'Sistemas de Datos',
+    'name': 'Planes de cuentas españoles (según PGCE 2008) ampliado para IGIC',
+    'version': '2.0',
+    'author': 'Sistemas de Datos',
     'maintainer': 'Sistemas de Datos',
-    'category' : 'Localisation/Account Charts',
+    'category': 'Localisation/Account Charts',
     'sequence': 3,
     'website': 'http://www.sdatos.com',
-    'depends' : ['l10n_es'],            
+    'depends': ['l10n_es'],
     'data': ['data/account_account_common_igic.xml',
              'data/account_tax_group_data.xml',
              'data/taxes_common_igic.xml',   
