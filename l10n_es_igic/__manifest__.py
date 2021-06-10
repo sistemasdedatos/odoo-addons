fa2cb9dc3c6fb34cd5ff0e0c58f66847fbae3f25
HéctorJotaPunto <hravelo@sdatos.es>
2021-06-10 14:00:07 +0100

    [UPD] l10n_es_igic: reincluido el impuesto de IGIC 7%

--- l10n_es_igic/__manifest__.py
+++ l10n_es_igic/__manifest__.py
@@ -1,28 +1,34 @@
-# -*- encoding: utf-8 -*-
 ##############################################################################
 #
 #    Sistemas de Datos, Open Source Management Solution
-#    Copyright (C) 2014 Arturo Esparragón Goncalves (http://sdatos.com). All Rights Reserved
-#    Copyright (C) 2016-2018 Rodrigo Colombo Vlaeminch (http://sdatos.com). All Rights Reserved
+#    Copyright (C) 2004-2011 Pexego Sistemas Informáticos.
+#    Copyright (C) 2014 Arturo Esparragón Goncalves (http://sdatos.com).
+#    Copyright (C) 2016-2018 Rodrigo Colombo Vlaeminch (http://sdatos.com).
+#    Copyright (C) 2019 Comunitea (https://comunitea.com).
+#    Copyright (C) 2019 Héctor J. Ravelo (http://sdatos.com)
 #    License AGPL-3 - See http://www.gnu.org/licenses/agpl-3.0
 #
 ##############################################################################
 
 {
-    'name' : 'Planes de cuentas españoles (según PGCE 2008) ampliado para IGIC',
-    'version' : '2.0',
-    'author' : 'Sistemas de Datos',
-    'maintainer': 'Sistemas de Datos',
-    'category' : 'Localisation/Account Charts',
-    'sequence': 3,
-    'website': 'http://www.sdatos.com',
-    'depends' : ['l10n_es'],            
-    'data': ['data/account_account_common_igic.xml',
+    'name': 'IGIC (Impuesto General Indirecto Canario',
+    'version': '12.0.1.0.1',
+    'author': "David Diz Martínez <daviddiz@gmail.com>,"
+              "Atlantux Consultores - Enrique Zanardi,"
+              "Sistemas de Datos,"
+              "Comunitea,"
+              "Odoo Community Association (OCA)",
+    'category': 'Localisation/Account Charts',
+    "website": "https://github.com/OCA/l10n-spain",
+    "depends": ['l10n_es'],
+    "license": "AGPL-3",
+    'data': ['data/account_group.xml',
+             'data/account_account_common_igic.xml',
              'data/account_tax_group_data.xml',
-             'data/taxes_common_igic.xml',   
+             'data/taxes_common_igic.xml',
              'data/fiscal_position_common_igic.xml'],
-    'installable': False,        
-    'auto_install': False,        
+    'installable': True,
+    'auto_install': False,
 }
 
 # vim:expandtab:smartindent:tabstop=4:softtabstop=4:shiftwidth=4:
